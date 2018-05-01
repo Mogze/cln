60578fbe557e2069e5db9e74f04012530ffd9107
zehreken <zehreken@gmail.com>
2018-05-01 21:44:55 +0300

    Improve camera follow

--- Assets/Sources/Features/CameraFeature/Systems/CameraMoveSystem.cs
+++ Assets/Sources/Features/CameraFeature/Systems/CameraMoveSystem.cs
@@ -3,24 +3,27 @@ using UnityEngine;
 
 namespace cln
 {
-    public sealed class CameraMoveSystem : IExecuteSystem
+    public sealed class CameraMoveSystem : IInitializeSystem, IExecuteSystem
     {
         private IContext<GameEntity> _context;
-        private readonly Transform _cameraTransform;
+        private Transform _cameraTransform;
         private GameEntity _cubeEntity;
 
         public CameraMoveSystem(IContext<GameEntity> context)
         {
             _context = context;
+        }
+
+        public void Initialize()
+        {
             _cameraTransform = Camera.main.transform;
-            _cubeEntity = context.GetGroup(GameMatcher.Cube).GetSingleEntity();
+            _cubeEntity = _context.GetGroup(GameMatcher.Cube).GetSingleEntity();
         }
 
         public void Execute()
         {
-            if (_cubeEntity == null)
-                _cubeEntity = _context.GetGroup(GameMatcher.Cube).GetSingleEntity();
-            _cameraTransform.position = new Vector3(_cubeEntity.position.value.x + 8f, _cubeEntity.position.value.y + 9f, -10f);
+            var diff = new Vector3(_cubeEntity.position.value.x + 8f, _cubeEntity.position.value.y + 9f, -10f) - _cameraTransform.position;
+            _cameraTransform.Translate(diff * 2f * Time.deltaTime);
         }
     }
 }
