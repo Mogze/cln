34353375b869f9637855715142f896a48df64ec0
zehreken <zehreken@gmail.com>
2018-06-15 00:15:09 +0300

    Add leantween

--- Assets/Sources/Scripts/Splash.cs
+++ Assets/Sources/Scripts/Splash.cs
@@ -1,14 +1,19 @@
 ﻿using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace cln
 {
     public class Splash : MonoBehaviour
     {
+        public GameObject Logo;
+        
         private void Start()
         {
             StartCoroutine(LoadMain());
+            Logo.transform.localScale = Vector3.one * 0.5f;
+            LeanTween.scale(Logo, Vector3.one, 0.5f);
         }
 
         private IEnumerator LoadMain()
