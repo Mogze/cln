153442ecc2f6452e007a77cef1eae6185ee3a567
zehreken <zehreken@gmail.com>
2018-07-23 20:19:19 +0300

    Add test ad ids

--- Assets/Sources/Services/AdService.cs
+++ Assets/Sources/Services/AdService.cs
@@ -8,11 +8,17 @@ namespace cln.Sources.Services
 {
     public class AdService : IService
     {
-        private const string AppId = "ca-app-pub-8389931397130414~7907377047";
-        private const string BannerId = "ca-app-pub-8389931397130414/3968132034";
-        private const string InterstitialId = "ca-app-pub-8389931397130414/4716521742";
+        // Game ad ads
+        private const string AppId = "app-id";
+        private const string BannerId = "banner-id";
+        private const string InterstitialId = "interstitial-id";
+        private const string InterstitialVideoId = "interstitial-video-id";
+        private const string RewardedVideoId = "rewarded-video-id";
+        // Test ad Ids
         private const string TestBannerId = "ca-app-pub-3940256099942544/6300978111";
         private const string TestInterstitialId = "ca-app-pub-3940256099942544/1033173712";
+        private const string TestInterstitialVideoId = "ca-app-pub-3940256099942544/8691691433";
+        private const string TestRewardedVideoId = "ca-app-pub-3940256099942544/5224354917";
         private BannerView _bannerView;
         private const float InterstitialPeriod = 30f; // in seconds
         private bool _interstitialTimerReady = true;
